a0249cfc3f1010342d22695e443deaca32707ccb
Alexis Cesar <alexis.cesar2017@outlook.com>
2022-03-28 10:06:44 -0300

    Added contact table structure

--- web/public/js/contacts_script.js
+++ web/public/js/contacts_script.js
@@ -1,3 +1,21 @@
 function showAlert() {
     alert('function called!')
 }
+
+let myTable = document.getElementById("contactTable");
+
+for (let i = 0; i < 10; i++) {
+    let myRow = myTable.insertRow(-1);
+
+    let cell = myRow.insertCell(0);
+    cell.appendChild(document.createTextNode("Test Contact"));
+
+    cell = myRow.insertCell(1);
+    cell.appendChild(document.createTextNode("test-email.com"));
+
+    cell = myRow.insertCell(2);
+    cell.appendChild(document.createTextNode("blocked"));
+
+    cell = myRow.insertCell(3);
+    cell.appendChild(document.createTextNode("UPDATE | DELETE | BLOCK"));
+}
