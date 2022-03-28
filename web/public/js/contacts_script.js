84a2cd7c4da47c293d8222d36270e698f1c84824
Alexis Cesar <alexis.almeida@programmers.com.br>
2022-03-28 11:33:35 -0300

    Add test contact number

--- web/public/js/contacts_script.js
+++ web/public/js/contacts_script.js
@@ -4,7 +4,7 @@ for (let i = 0; i < 10; i++) {
     let myRow = myTable.insertRow(-1);
 
     let cell = myRow.insertCell(0);
-    cell.appendChild(document.createTextNode("Test Contact"));
+    cell.appendChild(document.createTextNode(`Test Contact #${i + 1}`));
 
     cell = myRow.insertCell(1);
     cell.appendChild(document.createTextNode("test-email.com"));
