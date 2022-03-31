e3e2feb134d91875b511566080afd7e9deb6af0f
Alexis Cesar <alexis.cesar2017@outlook.com>
2022-03-30 21:16:35 -0300

    Prevent sql injection

--- api/contact_repository.py
+++ api/contact_repository.py
@@ -16,7 +16,7 @@ class ContactRepository():
         return self.__conn.cursor()
 
     def insert(self, first_name, last_name, email, birth_date):
-        self.__cursor.execute(f"INSERT INTO dbo.Contact(Id, FirstName, LastName, Email, BirthDate, Blocked) VALUES(NEWID(), \'{first_name}\', \'{last_name}\', \'{email}\', \'{birth_date}\', 0);")
+        self.__cursor.execute("INSERT INTO dbo.Contact(Id, FirstName, LastName, Email, BirthDate, Blocked) VALUES(NEWID(), ?, ?, ?, ?, 0);", first_name, last_name, email, birth_date)
         self.__conn.commit()
 
     def findAll(self):
@@ -29,15 +29,15 @@ class ContactRepository():
         return data
 
     def findById(self, contactId):
-        self.__cursor.execute(f'SELECT * FROM dbo.Contact WHERE Id = \'{contactId}\';')
+        self.__cursor.execute('SELECT * FROM dbo.Contact WHERE Id = ?;', contactId)
         record = self.__cursor.fetchone()
         column_names = [column[0] for column in self.__cursor.description]
         return dict(zip(column_names, record))
 
     def update(self, contactId, first_name, last_name, email, birth_date, blocked):
-        self.__cursor.execute(f'UPDATE dbo.Contact SET FirstName=\'{first_name}\', LastName=\'{last_name}\', Email=\'{email}\', BirthDate=\'{birth_date}\', Blocked=\'{blocked}\' WHERE Id = \'{contactId}\';')
+        self.__cursor.execute('UPDATE dbo.Contact SET FirstName=?, LastName=?, Email=?, BirthDate=?, Blocked=? WHERE Id = ?;', first_name, last_name, email, birth_date, blocked, contactId)
         self.__conn.commit()
 
     def delete(self, contactId):
-        self.__cursor.execute(f'DELETE FROM dbo.Contact WHERE Id = \'{contactId}\';')
+        self.__cursor.execute('DELETE FROM dbo.Contact WHERE Id = ?;', contactId)
         self.__conn.commit()
